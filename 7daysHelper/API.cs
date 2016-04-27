d76b1dbde6f2ebf3f6508498c5ffe7306f7a3f20
Josh Becker <beckerbi@gmail.com>
2016-04-27 19:44:34 -0400

    Initial commit

---
+++ 7daysHelper/API.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace _7daysHelper
+{
+
+    public class API : ModApiAbstract
+    {
+        public override bool ChatMessage(ClientInfo _cInfo, string _message, string _playerName)
+        {
+            return ChatHook.Hook(_cInfo, _message, _playerName);
+        }
+
+        //public override void GameAwake()
+        //{
+        //}
+
+        //public override void GameShutdown()
+        //{
+        //}
+
+        //public override void GameUpdate()
+        //{
+        //}
+
+        //public override void SavePlayerData(ClientInfo _cInfo, PlayerDataFile _playerDataFile)
+        //{
+        //}
+
+        //public override void PlayerLogin(ClientInfo _cInfo, string _compatibilityVersion)
+        //{
+        //}
+
+        //public override void PlayerSpawning(ClientInfo _cInfo, int _chunkViewDim, PlayerProfile _playerProfile)
+        //{
+        //}
+
+        //public override void PlayerDisconnected(ClientInfo _cInfo, bool _bShutdown)
+        //{
+        //}
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# 7DaysHelper
+So far this helper only adds the command "/7days". When a player enters this command they will be told how long until the day 7 horde spawns.
+
+
+## Installation
+
+1. On your dedicated server created a folder called Mods within the installation folder, example folder:
+
+```
+7DaysToDieServer_Data
+7DaysToDieServer.x86
+7DaysToDieServer.x86_64
+AudioLog.txt
+Data
+libstdc++.so.6
+Mods
+Saves
+serverconfig.xml
+startserver.sh
+steam_appid.txt
+steamapps
+```
+
+2. Download the latest release
